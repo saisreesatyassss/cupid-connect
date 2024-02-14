4d44dd961397702076e311bb182b30b5283dd691
saisreesatyassss <saisreesatyassss@gmail.com>
2024-02-14 12:21:12 +0530

    signin change

--- src/app/Main/page.tsx
+++ src/app/Main/page.tsx
@@ -58,7 +58,8 @@ const signIn = async () => {
     <section className="text-white h-screen relative">
       {/* Image */}
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-            <div className="logo-container" onClick={signIn}>
+            {/* <div className="logo-container" onClick={signIn}> */}
+            <div className="logo-container">
 
         {/* <Link href="/">  */}
  
